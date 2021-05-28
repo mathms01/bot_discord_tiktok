dff4d981ccc895bbee4e1878a945208d6e5f3d58
EDM <mathieu.mussot01@gmail.com>
2021-05-28 19:24:48 +0200

    [INIT] init project

---
+++ main.py
@@ -0,0 +1,47 @@
+#Bot discord pour récupérer des infos sur un compte tiktok
+#
+#Créé par @edeme_ sur Tiktok
+
+#impots diverses
+import discord
+from discord.ext import commands
+from TikTokApi import TikTokApi
+import nest_asyncio
+
+#initialisation de l'asynchrone
+nest_asyncio.apply()
+
+#template du message de réponse
+txtTemplate = "Information about {0}'s account :\n \
+                Followers : {1}\n \
+                Video count : {2}\n \
+                Heart count : {3}\n"
+
+#initialisation des varaibles
+bot = commands.Bot(command_prefix= "!", description = "Bot de edeme_")
+api = TikTokApi()
+# username = 'edeme_'
+
+#fonction de renvoi des inforamtions d'un utilisateur
+async def parse_User(userToFind):
+    user = api.get_instance().get_user(username = userToFind).get('userInfo').get('stats')
+    return txtTemplate.format(userToFind, user.get('followerCount'), user.get('videoCount'), user.get('heartCount'))
+
+#evénement d
+@bot.event
+async def on_ready():
+    print("Ready !")
+
+#événement de retour
+@bot.command()
+async def getUser(ctx, user):
+    user = await parse_User(user)
+    await ctx.send(user)
+
+#lancement du bot
+bot.run("ODQ2MTQxMjg3OTc1NDg1NDgw.YKrMew.wx4x-GgYxDQmgoGS5AE78Xs4UwM")
+
+
+
+
+
